2fa597c7122c66910ef0259f66e9d9a3f0a4df57
Lenildo Luan <lenildoluan@gmail.com>
2025-05-28 12:17:59 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Lenildo Luan',
+    default:
+      'Lenildo Luan - Desenvolvedor de software por formação e empreendedor por natureza',
+  },
+  description:
+    'Desenvolvedor de software por formação e empreendedor por natureza. Moro em João Pessoa, onde cofundei a Kalilu, um estúdio de tecnologia criativa focada em desenvolver soluções inovadoras.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
